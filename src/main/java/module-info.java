1fe2676988a2048ce52152231bde57130c89137d
Mayra Garcia <mayra.alejandra.garcia.garrido@uabc.edu.mx>
2025-10-16 11:35:54 -0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module costco.sim {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens costco.sim to javafx.fxml;
+    exports costco.sim;
+}
